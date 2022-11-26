e81de47bb70427a4971acc1fdf28903c594aaddc
Iaarun <automationbyaarun@gmail.com>
2022-11-26 14:50:32 +0530

    adding xpathdoc scroll and alert

--- src/seleniumScripts/Scripts.java
+++ src/seleniumScripts/Scripts.java
@@ -3,7 +3,9 @@ package seleniumScripts;
 import java.util.List;
 import java.util.Set;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,41 +23,84 @@ public class Scripts {
 	public static void main(String[] args) throws InterruptedException {
 		Scripts sc = new Scripts();
 		sc.launchBrowser();
-		sc.handleDropdown();
+		sc.handleAlert();
 	}
 	
-	public void handleDropdown() {
+	public void handleAlert() {
+		driver.get("http://www.tizag.com/javascriptT/javascriptprompt.php");
+		WebElement btn = driver.findElement(By.xpath("//input[@value='Say my name!']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView();", btn);
+		btn.click();
+		Alert alert = driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.sendKeys("Arun");
+		alert.accept();
+		System.out.println(alert.getText());
+		alert.dismiss();
+		
+		
+		
+	}
+
+	public void scroll() {
+		driver.get("http://www.tizag.com/javascriptT/javascriptalert.php");
+		WebElement btn = driver.findElement(By.xpath("//input[@value='Confirmation Alert']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		// random scroll
+		// js.executeScript("window.scrollBy(0,500)", "");
+		
+		// scroll till element into view
+	//	js.executeScript("arguments[0].scrollIntoView();", btn);
+		
+		//scroll till document height
+		
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)", "");
+		
+
+	}
+
+	public void handleDropdown() throws InterruptedException {
 		driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
-	 WebElement number=	driver.findElement(By.name("my-select"));
-	 
-	 Select select = new Select(number);
-	System.out.println(select.getFirstSelectedOption().getText());
-	
-	  List<WebElement> option=  select.getOptions();
-	  System.out.println("Number fo element : "+option.size() );
-	  
-	  option.forEach(x->{System.out.println(x.getText());});
-	  
-	  select.selectByVisibleText("Two");
-	  
-	  select.selectByValue("3");
-	  
-	  select.selectByIndex(0);
+		WebElement number = driver.findElement(By.name("my-select"));
+
+		Select select = new Select(number);
+		System.out.println(select.getFirstSelectedOption().getText());
+
+		List<WebElement> option = select.getOptions();
+		System.out.println("Number fo element : " + option.size());
+
+		option.forEach(x -> {
+			System.out.println(x.getText());
+		});
+		Thread.sleep(1000);
+
+		select.selectByVisibleText("Two");
+		Thread.sleep(1000);
+
+		select.selectByValue("3");
+		Thread.sleep(1000);
+
+		select.selectByIndex(0);
+
+		System.out.println(select.isMultiple());
 	}
-	
+
 	public void handlecheckboxandradioButton() {
 		driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
-		WebElement img= driver.findElement(By.className("img-fluid"));
+		WebElement img = driver.findElement(By.className("img-fluid"));
 		System.out.println(img.isDisplayed());
-		
-		WebElement check1= driver.findElement(By.id("my-check-1"));
-		 System.out.println(check1.isSelected()); //true
-		 check1.click();
-		 System.out.println(check1.isSelected());//false
-		 
-	WebElement disablebox=	 driver.findElement(By.name("my-disabled"));
-	
-	System.out.println(disablebox.isEnabled());
+
+		WebElement check1 = driver.findElement(By.id("my-check-1"));
+		System.out.println(check1.isSelected()); // true
+		check1.click();
+		System.out.println(check1.isSelected());// false
+
+		WebElement disablebox = driver.findElement(By.name("my-disabled"));
+
+		System.out.println(disablebox.isEnabled());
+
 	}
 
 	public void senddataandClickonLInk() {
